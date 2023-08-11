d0bb6ee73c835716a9b17f894d7e0bb0d4938581
Margo1212 <sirachenko944@gmail.com>
2023-08-11 20:42:25 +0200

    add images from aws

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -12,12 +12,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
       <div className="flex flex-col items-center w-full gap-1">
         <div className="aspect-square overflow-hidden relative w-full">
-          {/* <Image
-            src={data.images[0].image}
+          <Image
+            src={data.image}
             alt={data.name}
             fill
             className="w-full h-full object-contain"
-          /> */}
+          />
         </div>
         <div className="mt-4">{truncateText(data.name)}</div>
         <div>{data.model}</div>
